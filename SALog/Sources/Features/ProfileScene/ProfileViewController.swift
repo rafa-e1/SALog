393accb40317b0769fd1c17bbbae46a2759e7fea
RAFA <rafa71802937@gmail.com>
2025-01-05 16:28:58 +0900

    feat: 내비게이션 바 버튼 추가
    - 병영 주소 복사 기능으로 구현할 예정

--- SALog/Sources/Features/ProfileScene/ProfileViewController.swift
+++ SALog/Sources/Features/ProfileScene/ProfileViewController.swift
@@ -7,4 +7,23 @@
 
 import UIKit
 
-final class ProfileViewController: BaseViewController {}
+final class ProfileViewController: BaseViewController {
+
+    // MARK: - Properties
+
+    private let copyButton = UIButton(type: .system)
+
+    // MARK: - UI
+
+    override func setStyle() {
+        super.setStyle()
+
+        copyButton.do {
+            $0.setImage(UIImage(systemName: "doc.on.doc"), for: .normal)
+            $0.tintColor = .black
+        }
+
+        let rightBarButton = UIBarButtonItem(customView: copyButton)
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+}
