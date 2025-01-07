544edf398233e8b500a1bcfbcc43b22f8f36c449
RAFA <rafa71802937@gmail.com>
2025-01-07 17:29:30 +0900

    feat: 프로필 뷰 첫 번째 섹션 UI 구현

--- SALog/Sources/Features/ProfileScene/ProfileViewController.swift
+++ SALog/Sources/Features/ProfileScene/ProfileViewController.swift
@@ -12,6 +12,34 @@ final class ProfileViewController: BaseViewController {
     // MARK: - Properties
 
     private let copyButton = UIButton(type: .system)
+    private lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: createLayout()
+    )
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNavigationBarButtonItems()
+        setDelegates()
+        registerCells()
+    }
+
+    // MARK: - Helpers
+
+    private func setDelegates() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+    private func registerCells() {
+        collectionView.register(
+            ProfileCell.self,
+            forCellWithReuseIdentifier: ProfileCell.identifier
+        )
+    }
 
     // MARK: - UI
 
@@ -21,13 +49,84 @@ final class ProfileViewController: BaseViewController {
     }
 
     override func setStyle() {
-
         copyButton.do {
             $0.setImage(UIImage(systemName: "doc.on.doc"), for: .normal)
             $0.tintColor = .black
         }
 
-        let rightBarButton = UIBarButtonItem(customView: copyButton)
-        navigationItem.rightBarButtonItem = rightBarButton
+        collectionView.backgroundColor = .background
+    }
+
+    override func setHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    override func setLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfileViewController: UICollectionViewDataSource {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 1
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ProfileCell.identifier,
+            for: indexPath
+        ) as? ProfileCell else {
+            return UICollectionViewCell()
+        }
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfileViewController: UICollectionViewDelegate {}
+
+// MARK: - UICollectionViewCompositionalLayout
+
+private extension ProfileViewController {
+
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
+    }
+
+    func sectionProvider(
+        sectionIndex: Int,
+        environment: NSCollectionLayoutEnvironment
+    ) -> NSCollectionLayoutSection? {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(300)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(300)
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        return section
     }
 }
