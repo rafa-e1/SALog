9eb5160dde85f715d0ec5fcdc221499e914d29ba
RAFA <rafa71802937@gmail.com>
2025-01-12 02:32:40 +0900

    add: 프로필 메뉴 탭에 해당하는 셀 추가
    - 기본 정보 셀
    - 맵 숙련도 셀
    - 랭크전 정보 셀
    - 매칭 기록 셀

---
+++ SALog/Sources/Features/ProfileScene/View/ProfileMapLevelCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileMapLevelCell.swift
+//  SALog
+//
+//  Created by RAFA on 1/12/25.
+//
+
+import UIKit
+
+final class ProfileMapLevelCell: BaseCollectionViewCell {
+
+    // MARK: - UI
+
+    override func setStyle() {
+        contentView.backgroundColor = .systemGreen
+    }
+}
---
+++ SALog/Sources/Features/ProfileScene/View/ProfileRankInfoCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileRankInfoCell.swift
+//  SALog
+//
+//  Created by RAFA on 1/12/25.
+//
+
+import UIKit
+
+final class ProfileRankInfoCell: BaseCollectionViewCell {
+
+    // MARK: - UI
+
+    override func setStyle() {
+        contentView.backgroundColor = .systemYellow
+    }
+}
