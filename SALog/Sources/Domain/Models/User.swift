34e8a6f4c5bb461b80aa0fa4b87e9263a3cb2578
RAFA <rafa71802937@gmail.com>
2025-01-14 03:06:11 +0900

    feat: 유저 정보 모델 생성
    - 유저 고유 식별자(userNexonSN), 닉네임(userNickname), 프로필 이미지 URL(userImageURL)을 포함한 User 모델 추가
    - 서버에서 받아온 유저 데이터를 효율적으로 관리하기 위한 데이터 구조 설계

---
+++ SALog/Sources/Domain/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SALog
+//
+//  Created by RAFA on 1/13/25.
+//
+
+import Foundation
+
+struct User {
+    let userNexonSN: Int
+    let userNickname: String
+    let userImageURL: String
+}
