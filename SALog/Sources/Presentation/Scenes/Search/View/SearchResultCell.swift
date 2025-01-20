794aa732c48067fff1a5d4066bb6567bce2b2446
RAFA <rafa71802937@gmail.com>
2025-01-20 17:45:07 +0900

    feat: Add nickname label to SearchResultCell

--- SALog/Sources/Presentation/Scenes/Search/View/SearchResultCell.swift
+++ SALog/Sources/Presentation/Scenes/Search/View/SearchResultCell.swift
@@ -9,9 +9,36 @@ import UIKit
 
 final class SearchResultCell: BaseTableViewCell {
 
+    // MARK: - Properties
+
+    private let nicknameLabel = UILabel()
+
+    // MARK: - Helpers
+
+    func configure(_ user: User) {
+        nicknameLabel.text = user.userNickname
+    }
+
     // MARK: - UI
 
     override func setStyle() {
         super.setStyle()
+
+        nicknameLabel.do {
+            $0.font = .systemFont(ofSize: 17, weight: .medium)
+            $0.textColor = .black
+        }
+    }
+
+    override func setHierarchy() {
+        contentView.addSubview(nicknameLabel)
+    }
+
+    override func setLayout() {
+        nicknameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.verticalEdges.equalToSuperview().inset(10)
+            $0.left.equalTo(16)
+        }
     }
 }
