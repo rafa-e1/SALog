23ee49948e1636d5e12e22d8da84b5a69533e544
RAFA <rafa71802937@gmail.com>
2025-01-26 21:33:29 +0900

    feat: Add search by clan name functionality to SearchService

--- SALog/Sources/Data/Network/Search/SearchService.swift
+++ SALog/Sources/Data/Network/Search/SearchService.swift
@@ -12,6 +12,11 @@ protocol SearchServiceProtocol {
         _ nickname: String,
         completion: @escaping (Result<[User], Error>) -> Void
     )
+
+    func searchByClanName(
+        _ clanName: String,
+        completion: @escaping (Result<[Clan], Error>) -> Void
+    )
 }
 
 final class SearchService: SearchServiceProtocol {
@@ -28,7 +33,7 @@ final class SearchService: SearchServiceProtocol {
     ) {
         networkService.request(
             SearchTarget.searchByNickname(nickname: nickname)
-        ) { (result: Result<BaseResponse<SearchResponseDTO>, Error>) in
+        ) { (result: Result<BaseResponse<NicknameSearchResponseDTO>, Error>) in
             switch result {
             case .success(let response):
                 guard let characterInfo = response.result?.characterInfo,
@@ -41,6 +46,32 @@ final class SearchService: SearchServiceProtocol {
 
                 let characters = characterInfo.map { $0.toDomain() }
                 completion(.success(characters))
+            case .failure(let error):
+                print("DEBUG: 에러 발생 - \(error.localizedDescription)")
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func searchByClanName(
+        _ clanName: String,
+        completion: @escaping (Result<[Clan], any Error>) -> Void
+    ) {
+        networkService.request(
+            SearchTarget.searchByClan(clanName: clanName)
+        ) { (result: Result<BaseResponse<ClanNameSearchResponseDTO>, Error>) in
+            switch result {
+            case .success(let response):
+                guard let clanInfo = response.result?.clanInfo,
+                      !clanInfo.isEmpty
+                else {
+                    print("DEBUG: 클랜을 찾을 수 없습니다.")
+                    completion(.success([]))
+                    return
+                }
+
+                let clans = clanInfo.map { $0.toDomain() }
+                completion(.success(clans))
             case .failure(let error):
                 print("DEBUG: 에러 발생 - \(error.localizedDescription)")
                 completion(.failure(error))
