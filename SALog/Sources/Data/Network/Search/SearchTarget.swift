ddcf56776b0404cb6a87299a443143af9239c10b
RAFA <rafa71802937@gmail.com>
2025-01-26 21:27:54 +0900

    refactor: task 타입 단순화

--- SALog/Sources/Data/Network/Search/SearchTarget.swift
+++ SALog/Sources/Data/Network/Search/SearchTarget.swift
@@ -32,12 +32,7 @@ extension SearchTarget: TargetType {
     }
 
     var task: Task {
-        switch self {
-        case .searchByNickname:
-            return .requestPlain
-        case .searchByClan:
-            return .requestPlain
-        }
+        return .requestPlain
     }
 
     var headers: [String: String]? {
