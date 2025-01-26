b0372337a234c56050940f5a286f6f95416ade9d
RAFA <rafa71802937@gmail.com>
2025-01-26 21:35:37 +0900

    feat: Update SearchResultCell to support both user and clan search types

--- SALog/Sources/Presentation/Scenes/Search/View/SearchResultCell.swift
+++ SALog/Sources/Presentation/Scenes/Search/View/SearchResultCell.swift
@@ -11,12 +11,21 @@ final class SearchResultCell: BaseTableViewCell {
 
     // MARK: - Properties
 
-    private let nicknameLabel = UILabel()
+    private let nameLabel = UILabel()
 
     // MARK: - Helpers
 
-    func configure(_ user: User) {
-        nicknameLabel.text = user.userNickname
+    func configure(type: SearchType, user: User? = nil, clan: Clan? = nil) {
+        switch type {
+        case .nickname:
+            guard let user = user else { return }
+
+            nameLabel.text = user.userNickname
+        case .clan:
+            guard let clan = clan else { return }
+
+            nameLabel.text = clan.clanName
+        }
     }
 
     // MARK: - UI
@@ -24,18 +33,18 @@ final class SearchResultCell: BaseTableViewCell {
     override func setStyle() {
         super.setStyle()
 
-        nicknameLabel.do {
+        nameLabel.do {
             $0.font = .systemFont(ofSize: 17, weight: .medium)
             $0.textColor = .black
         }
     }
 
     override func setHierarchy() {
-        contentView.addSubview(nicknameLabel)
+        contentView.addSubview(nameLabel)
     }
 
     override func setLayout() {
-        nicknameLabel.snp.makeConstraints {
+        nameLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.verticalEdges.equalToSuperview().inset(10)
             $0.left.equalTo(16)
