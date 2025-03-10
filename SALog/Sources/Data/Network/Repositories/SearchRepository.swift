5152a930204dd6f969270407d2bb1309e4a41c24
RAFA <rafa71802937@gmail.com>
2025-03-10 17:35:59 +0900

    feat: SearchRepository 구현

---
+++ SALog/Sources/Data/Network/Repositories/SearchRepository.swift
@@ -0,0 +1,25 @@
+//
+//  SearchRepository.swift
+//  SALog
+//
+//  Created by RAFA on 3/10/25.
+//
+
+import Foundation
+
+final class SearchRepository: SearchRepositoryProtocol {
+
+    private let network: NetworkServiceProtocol
+
+    init(network: NetworkServiceProtocol) {
+        self.network = network
+    }
+
+    func searchByNickname(_ nickname: String) async throws -> SearchResult {
+        let response: SearchResultResponseDTO = try await network.request(
+            SearchTarget.searchByNickname(nickname: nickname)
+        )
+
+        return response.toDomain()
+    }
+}
