efd0eff57cf956ec7baedd0994723203962d19be
RAFA <rafa71802937@gmail.com>
2025-03-10 15:56:37 +0900

    feat: SearchRepositoryProtocol 구현

---
+++ SALog/Sources/Domain/Repositories/SearchRepositoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SearchRepositoryProtocol.swift
+//  SALog
+//
+//  Created by RAFA on 3/10/25.
+//
+
+import Foundation
+
+protocol SearchRepositoryProtocol {
+    func searchByNickname(_ nickname: String) async throws -> SearchResult
+}
