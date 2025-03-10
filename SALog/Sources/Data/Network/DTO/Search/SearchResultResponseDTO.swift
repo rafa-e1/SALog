10bd58567296b8d3bd1c1371b8f4421e3b5aa809
RAFA <rafa71802937@gmail.com>
2025-03-10 15:55:39 +0900

    feat: SearchResultResponseDTO 구현

---
+++ SALog/Sources/Data/Network/DTO/Search/SearchResultResponseDTO.swift
@@ -0,0 +1,50 @@
+//
+//  SearchResultResponseDTO.swift
+//  SALog
+//
+//  Created by RAFA on 3/10/25.
+//
+
+import Foundation
+
+struct SearchResultResponseDTO: Decodable {
+    let rtnCode: Int
+    let message: String?
+    let result: CharacterListResultResponseDTO
+
+    enum CodingKeys: CodingKey {
+        case rtnCode
+        case message
+        case result
+    }
+}
+
+struct CharacterListResultResponseDTO: Decodable {
+    let characterInfo: [CharacterInfoResponseDTO]
+    let totalCount: Int
+    let pageNumber: Int
+
+    enum CodingKeys: String, CodingKey {
+        case characterInfo = "characterInfo"
+        case totalCount = "total_cnt"
+        case pageNumber = "page_no"
+    }
+}
+
+extension SearchResultResponseDTO {
+
+    func toDomain() -> SearchResult {
+        return .init(rtnCode: rtnCode, message: message, result: result.toDomain())
+    }
+}
+
+extension CharacterListResultResponseDTO {
+
+    func toDomain()-> CharacterListResult {
+        return .init(
+            characterInfo: characterInfo.map { $0.toDomain() },
+            totalCount: totalCount,
+            pageNumber: pageNumber
+        )
+    }
+}
