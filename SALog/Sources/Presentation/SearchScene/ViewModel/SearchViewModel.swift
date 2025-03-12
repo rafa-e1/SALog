edd5d904654ba9d1d314c8ab3e94b2f58768d81d
RAFA <rafa71802937@gmail.com>
2025-03-12 10:43:30 +0900

    refactor: SearchViewModel에 input/output 패턴 및 protocols 구현

--- SALog/Sources/Presentation/SearchScene/ViewModel/SearchViewModel.swift
+++ SALog/Sources/Presentation/SearchScene/ViewModel/SearchViewModel.swift
@@ -8,11 +8,41 @@
 import RxCocoa
 import RxSwift
 
+protocol SearchViewModelInput {
+    var searchType: Observable<SearchType> { get }
+    var query: Observable<String> { get }
+    var selectResult: Observable<SearchResultType> { get }
+}
+
+protocol SearchViewModelOutput {
+    var results: Observable<[SearchResultType]> { get }
+    var filteredResults: Observable<[SearchResultType]> { get }
+    var isLoading: Observable<Bool> { get }
+    var errorMessage: Observable<String> { get }
+}
+
 protocol SearchViewModelProtocol {
-    func transform(input: SearchViewModel.Input) -> SearchViewModel.Output
+    func transform(input: SearchViewModelInput) -> SearchViewModelOutput
 }
 
 final class SearchViewModel: SearchViewModelProtocol {
+
+    // MARK: - Input
+
+    struct Input: SearchViewModelInput {
+        let searchType: Observable<SearchType>
+        let query: Observable<String>
+        let selectResult: Observable<SearchResultType>
+    }
+
+    // MARK: - Output
+
+    struct Output: SearchViewModelOutput {
+        let results: Observable<[SearchResultType]>
+        let filteredResults: Observable<[SearchResultType]>
+        let isLoading: Observable<Bool>
+        let errorMessage: Observable<String>
+    }
 
     // MARK: - Properties
 
@@ -30,32 +60,17 @@ final class SearchViewModel: SearchViewModelProtocol {
         self.useCase = useCase
     }
 
-    // MARK: - Input
-
-    struct Input {
-        let searchType: Observable<SearchType>
-        let query: Observable<String>
-    }
-
-    // MARK: - Output
-
-    struct Output {
-        let results: Observable<[SearchResultType]>
-        let isLoading: Observable<Bool>
-        let errorMessage: Observable<String>
-    }
-
     // MARK: - Helpers
 
-    func transform(input: Input) -> Output {
+    func transform(input: SearchViewModelInput) -> SearchViewModelOutput {
         Observable
             .combineLatest(
                 input.searchType,
-                input.query.debounce(.milliseconds(300), scheduler: MainScheduler.instance)
+                input.query.debounce(
+                    .milliseconds(300),
+                    scheduler: MainScheduler.instance
+                )
             )
-            .do(onNext: { [weak self] _, _ in
-                self?.isLoadingRelay.accept(true)
-            })
             .flatMapLatest { [weak self] type, query -> Observable<[SearchResultType]> in
                 guard let self = self else { return .just([]) }
 
@@ -81,8 +96,28 @@ final class SearchViewModel: SearchViewModelProtocol {
             .bind(to: searchResultsRelay)
             .disposed(by: disposeBag)
 
+        input.selectResult
+            .subscribe(onNext: { [weak self] result in
+                self?.handleResultsSelection(result)
+            })
+            .disposed(by: disposeBag)
+
+        let filteredResults: Observable<[SearchResultType]> = Observable.combineLatest(
+            searchResultsRelay.asObservable(),
+            input.searchType
+        ).map { [weak self] results, type in
+            guard let self = self else { return [] }
+
+            return results.filter {
+                $0.isMatchingType(type)
+            }.sorted {
+                self.sortSearchResults($0, $1)
+            }
+        }
+
         return Output(
             results: searchResultsRelay.asObservable(),
+            filteredResults: filteredResults,
             isLoading: isLoadingRelay.asObservable(),
             errorMessage: errorMessageRelay.asObservable()
         )
@@ -108,10 +143,11 @@ final class SearchViewModel: SearchViewModelProtocol {
             Task {
                 do {
                     let result = try await self.useCase.searchByNickname(query)
-                    observer.onNext(result.result.characterInfo.map {
+                    let searchResults = result.result.characterInfo.map {
                         SearchResultType.nickname($0)
-                    })
+                    }
 
+                    observer.onNext(searchResults)
                     observer.onCompleted()
                 } catch {
                     self.errorMessageRelay.accept(error.localizedDescription)
@@ -134,10 +170,11 @@ final class SearchViewModel: SearchViewModelProtocol {
             Task {
                 do {
                     let result = try await self.useCase.searchByClan(query)
-                    observer.onNext(result.result.clanInfo.map {
+                    let searchResults = result.result.clanInfo.map {
                         SearchResultType.clan($0)
-                    })
+                    }
 
+                    observer.onNext(searchResults)
                     observer.onCompleted()
                 } catch {
                     self.errorMessageRelay.accept(error.localizedDescription)
@@ -147,6 +184,29 @@ final class SearchViewModel: SearchViewModelProtocol {
             }
 
             return Disposables.create()
+        }
+    }
+
+    private func sortSearchResults(
+        _ lhs: SearchResultType,
+        _ rhs: SearchResultType
+    ) -> Bool {
+        switch (lhs, rhs) {
+        case (.nickname(let lhsInfo), .nickname(let rhsInfo)):
+            return lhsInfo.userNexonSN < rhsInfo.userNexonSN
+        case (.clan(let lhsInfo), .clan(let rhsInfo)):
+            return lhsInfo.clanID < rhsInfo.clanID
+        default:
+            return false
+        }
+    }
+
+    private func handleResultsSelection(_ result: SearchResultType) {
+        switch result {
+        case .nickname(let characterInfo):
+            print("DEBUG: \(characterInfo.userNickname)")
+        case .clan(let clanInfo):
+            print("DEBUG: \(clanInfo.clanName)")
         }
     }
 }
