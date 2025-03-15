db36b0cc3cb349f11c04789d0f43f7c27f28d829
RAFA <rafa71802937@gmail.com>
2025-03-15 18:43:36 +0900

    refactor: POP 및 Input/Output 패턴 적용

--- SALog/Sources/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
+++ SALog/Sources/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
@@ -5,22 +5,57 @@
 //  Created by RAFA on 1/12/25.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
 
-final class ProfileViewModel {
+protocol ProfileViewModelInput {
+    var selectTab: Observable<ProfileMenuTab> { get }
+}
+
+protocol ProfileViewModelOutput {
+    var selectedTab: Observable<ProfileMenuTab> { get }
+}
+
+protocol ProfileViewModelProtocol {
+    func transform(input: ProfileViewModelInput) -> ProfileViewModelOutput
+}
+
+final class ProfileViewModel: ProfileViewModelProtocol {
+
+    // MARK: - Input
+
+    struct Input: ProfileViewModelInput {
+        let selectTab: Observable<ProfileMenuTab>
+    }
+
+    // MARK: - Output
+
+    struct Output: ProfileViewModelOutput {
+        let selectedTab: Observable<ProfileMenuTab>
+    }
 
     // MARK: - Properties
 
-    private(set) var selectedTab: ProfileMenuTab = .basicInfo {
-        didSet { onTabChanged?(selectedTab) }
-    }
+    private let selectedTabRelay = BehaviorRelay<ProfileMenuTab>(value: .basicInfo)
 
-    var onTabChanged: ((ProfileMenuTab) -> Void)?
+    private let disposeBag = DisposeBag()
 
     // MARK: - Helpers
 
-    func changeTab(to tab: ProfileMenuTab) {
-        guard selectedTab != tab else { return }
-        selectedTab = tab
+    func transform(input: ProfileViewModelInput) -> ProfileViewModelOutput {
+        configureTabSelection(from: input)
+
+        return Output(selectedTab: selectedTabRelay.asObservable())
+    }
+}
+
+// MARK: - Configuration
+
+private extension ProfileViewModel {
+
+    func configureTabSelection(from input: ProfileViewModelInput) {
+        input.selectTab
+            .bind(to: selectedTabRelay)
+            .disposed(by: disposeBag)
     }
 }
